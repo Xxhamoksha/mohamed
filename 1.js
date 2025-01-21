f0931e8e9fb5f73b51d57981da40e08b1d4066ac
Xxhamoksha <xxhamoksha90@gmail.com>
2025-01-20 19:43:33 -0800

    start

---
+++ 1.js
@@ -0,0 +1,19 @@
+const students = ["mohamed", "omar", "ahmed", "hamza"];
+for (let index = 0; index < students.length; index++) {
+    const element = students[index];
+    console.log("students:" + element);
+    
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
